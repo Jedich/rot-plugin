c0366e69aa96b0e610c871a35def0b8d3856c0b1
Jedich <alexyudakov483@gmail.com>
2021-03-30 11:58:10 +0300

    claimed chunks visualization

--- src/tld/sofugames/rot/Main.java
+++ src/tld/sofugames/rot/Main.java
@@ -1,11 +1,10 @@
 package tld.sofugames.rot;
 
 import com.mysql.jdbc.*;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.World;
+import net.minecraft.server.v1_16_R3.EntityPlayer;
+import org.bukkit.*;
 import org.bukkit.command.*;
+import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,8 +19,8 @@ import java.util.*;
 
 public class Main extends JavaPlugin {
 
-	final String username="root"; // Enter in your db username
-	final String password=""; // Enter your password for the db
+	final String username = "root"; // Enter in your db username
+	final String password = ""; // Enter your password for the db
 	final String url = "jdbc:mysql://localhost:3306/rotr"; // Enter URL with db name
 
 	//Connection vars
@@ -37,6 +36,7 @@ public class Main extends JavaPlugin {
 	public int getLastKing() {
 		return lastKing++;
 	}
+
 	@Override
 	public void onEnable() {
 		//TODO: db tables and checking for new data
@@ -73,8 +73,7 @@ public class Main extends JavaPlugin {
 				));
 				lastKing++;
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
@@ -100,15 +99,14 @@ public class Main extends JavaPlugin {
 					King thisKing = new King(getLastKing(), player, homeChunk);
 					kingData.put(sender.getName(), thisKing);
 					try {
-						if(thisKing.pushToDb(connection)) {
+						if (thisKing.pushToDb(connection)) {
 							sender.sendMessage("Chunk successfully claimed!" + ChatColor.GOLD + " You are now a King.");
 							sender.sendMessage("Please, name your kingdom with /kingdom setname [NAME]");
 						}
 					} catch (SQLException e) {
 						e.printStackTrace();
 					}
-				}
-				else {
+				} else {
 					//TODO: Check for neighboring chunks
 					sender.sendMessage("Chunk successfully claimed!");
 					ClaimedChunk claim = new ClaimedChunk(getLastClaim(), chunkName,
@@ -120,68 +118,89 @@ public class Main extends JavaPlugin {
 						e.printStackTrace();
 					}
 				}
-			}
-			else {
+			} else {
 				sender.sendMessage(ChatColor.RED + "Chunk is already claimed.");
 			}
 			return true;
-		}
-		else if(command.getName().equalsIgnoreCase("kingdom")) {
-			if(args[0].equalsIgnoreCase("setname")) {
+		} else if (command.getName().equalsIgnoreCase("kingdom")) {
+			if (args[0].equalsIgnoreCase("setname")) {
 				try {
 					kingData.get(sender.getName()).kingdomName = args[1];
-					kingData.get(sender.getName()).updateInDb(connection, new String[] {"kingdom_name"});
+					kingData.get(sender.getName()).updateInDb(connection, new String[]{"kingdom_name"});
 				} catch (SQLException e) {
 					sender.sendMessage("Update execution error");
 					e.printStackTrace();
 				}
-			}
-			else if(args[0].equalsIgnoreCase("info")) {
+			} else if (args[0].equalsIgnoreCase("info")) {
 				Player player = getServer().getPlayer(sender.getName());
 				String chunkName = player.getLocation().getChunk().toString();
-				if(!kingData.containsKey(sender.getName())) {
+				if (!kingData.containsKey(sender.getName())) {
 					sender.sendMessage("You are not a king yet! Type your first /claim to become a king!");
 				} else {
 					King thisKing = kingData.get(sender.getName());
-					sender.sendMessage(thisKing.kingdomName + ", the kingdom of " + sender.getName());
-					sender.sendMessage("Ruler level: " + thisKing.kingdomLevel);
+					sender.sendMessage((ChatColor.GOLD + thisKing.kingdomName) +
+							ChatColor.WHITE + ", the kingdom of " + ChatColor.GOLD + sender.getName());
+					sender.sendMessage("Kingdom level: " + thisKing.kingdomLevel);
 					sender.sendMessage("Total chunks: " + thisKing.chunkNumber);
-					if(chunkName.equals(thisKing.homeChunk.chunkId)) {
+					if (chunkName.equals(thisKing.homeChunk.chunkId)) {
 						sender.sendMessage("This is the ruler's home chunk.");
 					}
 				}
+			} else if (args[0].equalsIgnoreCase("show")) {
+				if (!kingData.containsKey(sender.getName())) {
+					sender.sendMessage("You are not a king yet! Type your first /claim to become a king!");
+				} else {
+					Player player = getServer().getPlayer(sender.getName());
+					String chunkName = player.getLocation().getChunk().toString();
+					for (Map.Entry<String, ClaimedChunk> chunk : claimData.entrySet()) {
+						ClaimedChunk ch = chunk.getValue();
+						int a = ch.world.getX() * 16;
+						int b = ch.world.getZ() * 16;
+						if (chunk.getValue().owner.equals(sender.getName())) {
+							for (int x = 0; x < 16; x++) {
+								for (int z = 0; z < 16; z++) {
+									World world = player.getWorld();
+									player.spawnParticle(Particle.VILLAGER_HAPPY,
+											new Location(world, a + x + 0.5f, world.getHighestBlockAt(a + x, b + z).getY()+1, b + z + 0.5f),
+											1, 0, 0, 0);
+								}
+							}
+						}
+					}
+				}
+			} else {
+				sender.sendMessage("/kingdom setname [NAME], info");
 			}
-		}
-		else if(command.getName().equalsIgnoreCase("reset")) {
+			return true;
+		} else if (command.getName().equalsIgnoreCase("reset")) {
 			claimData.clear();
 			kingData.clear();
 			onDisable();
 			onEnable();
 			return true;
-		}
-		else if(command.getName().equalsIgnoreCase("chunk-info")) {
+		} else if (command.getName().equalsIgnoreCase("chunk-info")) {
 			Player player = getServer().getPlayer(sender.getName());
 			String chunkName = player.getLocation().getChunk().toString();
 			String ownerName = "None";
 			sender.sendMessage("Current chunk: " + chunkName);
-			if(claimData.get(chunkName) != null) {
+			if (claimData.get(chunkName) != null) {
 				sender.sendMessage("Current owner: " + claimData.get(chunkName).owner);
-			}
-			else {
-				sender.sendMessage("The chunk is available for conquer!");
+			} else {
+				sender.sendMessage("The chunk is available for conquest!");
 			}
 			return true;
 		}
 		return false;
 	}
 
+
 	@Override
 	public void onDisable() {
 		try {
-			if (connection!=null && !connection.isClosed()){
+			if (connection != null && !connection.isClosed()) {
 				connection.close();
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
